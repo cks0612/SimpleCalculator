bd2968bbff8c30afccbe79cb024ceaad0383d88f
양채현 <2423978@naver.com>
2026-03-26 16:26:44 +0900

    과제2 코딩과 테스트 완료 -,*,/ 버튼 기능 완성 한 식에 여러가지 연산자(+, -, *, /)를 사용하더라도 계산 가능

--- Form1.cs
+++ Form1.cs
@@ -5,24 +5,31 @@ namespace SimpleCalculator
     public partial class Form1 : Form
     {
 
-
-        string currentInput = "";
-        string expression = "";
-  
-        double firstNumber = 0;  
-        bool isPlusClicked = false; 
+      
+        private string currentInput = "";
+        double firstNumber = 0;
+        string operation = "";
+        List<double> numbers = new List<double>();
+        List<string> operators = new List<string>();
+        
 
         private void NumberInput(string num)
         {
-           
             currentInput += num;
-
-            txtbx2.Text = currentInput;     
-            txtbx1.Text += num;
+            txtbx2.Text = currentInput; 
         }
 
+        private void OperatorInput(string op)
+        {
+            if (currentInput == "") return;
 
-        
+            numbers.Add(double.Parse(currentInput));
+            operators.Add(op);
+
+            txtbx1.Text += currentInput + " " + op + " ";
+            currentInput = "";
+        }
+
 
         public Form1()
         {
@@ -62,7 +69,7 @@ namespace SimpleCalculator
 
         private void dvbtn_Click(object sender, EventArgs e)
         {
-
+            OperatorInput("/");
         }
 
         private void num7btn_Click(object sender, EventArgs e)
@@ -117,44 +124,60 @@ namespace SimpleCalculator
 
         private void rstbtn_Click(object sender, EventArgs e)
         {
-          
-            if (!isPlusClicked || currentInput == "") return;
+            if (currentInput == "") return;
 
-            double secondNumber = double.Parse(currentInput);
-            double result = firstNumber + secondNumber;
+            numbers.Add(double.Parse(currentInput));
 
-    
-            txtbx1.Text = firstNumber + " + " + secondNumber + " = " + result;
+            // 1단계: * / 먼저 처리
+            for (int i = 0; i < operators.Count; i++)
+            {
+                if (operators[i] == "*" || operators[i] == "/")
+                {
+                    double result = 0;
+                    if (operators[i] == "*")
+                        result = numbers[i] * numbers[i + 1];
+                    else
+                        result = numbers[i] / numbers[i + 1];
 
+                    numbers[i] = result;
+                    numbers.RemoveAt(i + 1);
+                    operators.RemoveAt(i);
+                    i--;
+                }
+            }
 
-            txtbx2.Text = result.ToString();
+            // 2단계: + - 처리
+            double finalResult = numbers[0];
+            for (int i = 0; i < operators.Count; i++)
+            {
+                if (operators[i] == "+")
+                    finalResult += numbers[i + 1];
+                else
+                    finalResult -= numbers[i + 1];
+            }
 
-            currentInput = result.ToString();
-            isPlusClicked = false;
-        
-          
-        
+            txtbx1.Text += currentInput + " = " + finalResult;
+            txtbx2.Text = finalResult.ToString();
+
+            // 다음 계산을 위해 초기화
+            numbers.Clear();
+            operators.Clear();
+            currentInput = finalResult.ToString();
         }
 
         private void pbtn_Click(object sender, EventArgs e)
         {
-            if (currentInput == "") return;
-
-            firstNumber = double.Parse(currentInput);
-            isPlusClicked = true;
-
-            txtbx1.Text += " + ";
-            currentInput = "";
+            OperatorInput("+");
         }
 
         private void mbtn_Click(object sender, EventArgs e)
         {
-
+            OperatorInput("-");
         }
 
         private void mtpbtn_Click(object sender, EventArgs e)
         {
-
+            OperatorInput("*");
         }
 
         private void txtbx2_TextChanged(object sender, EventArgs e)
